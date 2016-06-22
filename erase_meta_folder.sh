fcbc1348014b1f673ba90b80babaac3d3c6f073c
dimonik <dimonik@letiko.com>
2016-06-22 17:15:35 +0200

    case-insensivity and minor changes

--- erase_meta_folder.sh
+++ erase_meta_folder.sh
@@ -8,7 +8,7 @@ SEARCH_DIR="$BASE_DIR/metadata_example/img"
 
 
 cd "$SEARCH_DIR"
-ALL_FILES=$(find . -maxdepth 10 -type f \( -name '*.png' -or -name '*.jpg' \) -print)
+ALL_FILES=$(find . -maxdepth 10 -type f \( -iname '*.png' -or -iname '*.jpg' \) -print)
 for entry in $ALL_FILES;
 do
 	printf "\n"
@@ -16,7 +16,8 @@ do
         exiftool -All $(basename $entry)
 	printf "\nErasing metadata..."
         exiftool -overwrite_original -all= $(basename $entry)
-	printf "\nDONE"
-	printf "\n"
+	printf "\nErased.\n"
 	read -p "To check the next item press [Enter]"
 done
+printf "\nFINISHED\n"
+echo "Press [ENTER] to exit." && read
